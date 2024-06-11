304ab3643592da994a985f278951dd89ce6fc2c7
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-06-12 00:26:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,18 @@ axios
 
       let content = `<div class="card shadow ">
                         <div class="card-header">
-                            <img class="rounded-circle border border-2" src="./profile-pics/profilepic.webp" alt=""
+                            <img class="rounded-circle border border-2" src="${
+                              post.author.profile_image
+                            }" alt=""
                                 style="width: 40px; height: 40px;">
-                            <b>belal ahmed</b>
+                            <b>${post.author.name}</b>
                         </div>
                         <div class="card-body">
-                            <img class="w-100" src="./placeholders/ainz-ooal-gown-overlord-thumb.jpg" alt="">
-                            <h6 class="mt-2" style="color: rgb(163, 159, 159);">2 min ago</h6>
-                            <h5>Best Anime Ever</h5>
+                            <img class="w-100" src="${post.image}" alt="">
+                            <h6 class="mt-2" style="color: rgb(163, 159, 159);">${
+                              post.created_at
+                            }</h6>
+                            <h5>${post.title === null ? "" : post.title}</h5> 
                             <p>${post.body}</p>
                             <hr>
                             <div>
@@ -26,7 +30,7 @@ axios
                                     <path
                                         d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
                                 </svg>
-                                <span>(3) comments</span>
+                                <span>(${post.comments_count}) comments</span>
                             </div>
                         </div>
                     </div>
