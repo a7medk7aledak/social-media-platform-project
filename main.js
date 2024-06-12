cabbbd2e2adbc7ab0d04235b8a6eff036cd36266
Belal Ahmed <belal.ahmed121sq1@gmail.com>
2024-06-12 14:56:40 +0300

    ابلع

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ axios
     let posts = response.data.data;
     document.getElementById("posts").innerHTML = ""; // to make posts empty
     for (const post of posts) {
+      console.log(post);
+
       let content = `<div class="card shadow ">
                         <div class="card-header">
                             <img class="rounded-circle border border-2" src="${
@@ -31,15 +33,27 @@ axios
                                         d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
                                 </svg>
                                 <span>(${post.comments_count}) comments</span>
+                                <span>(${post.comments_count}) comments
+                                  <span id="post-tags-${post.id}">
+                                    <button class= "btn btn-sm rounded-5" style="background-color: gray;color:#ffff">policy</button>
+                                  </span>
+                                </span>
                             </div>
                         </div>
                     </div>
         `;
       document.getElementById("posts").innerHTML += content; //+= to update data in loop
+      let currentPostTagsId = `post-tags-${post.id}`;
+      document.getElementById(currentPostTagsId).innerHTML = "";
+      for (const tag of post.tags) {
+        let tagsContent = ` <button class= "btn btn-sm rounded-5" style="background-color: gray;color:#ffff">${tag.name}</button>
+        `;
+        document.getElementById(currentPostTagsId).innerHTML += tagsContent;
+      }
     }
   })
   .catch(function (error) {
-    // handle error
+    axios;
     console.log(error);
   });
 
