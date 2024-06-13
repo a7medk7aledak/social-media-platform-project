4bae685e742764285e2d33b20254a6ed37d7cdee
Belal Ahmed <belal.ahmed121sq1@gmail.com>
2024-06-14 00:17:33 +0300

    edit & delete to profile to XD

--- profileScript.js
+++ profileScript.js
@@ -30,24 +30,53 @@ function getUser() {
 
 function getPosts() {
   const id = getCurrentUserId();
+  toggleLoader(true);
 
   axios
     .get(`https://tarmeezacademy.com/api/v1/users/${id}/posts`)
     .then((response) => {
+      toggleLoader(false);
+
       const posts = response.data.data;
       document.getElementById("user-posts").innerHTML = "";
       for (post of posts) {
         console.log(post);
 
+        let user = getCrrentUser();
+        let isMyPost = user != null && post.author.id == user.id;
+        let editBtnContent = ``;
+
+        if (isMyPost) {
+          editBtnContent = `
+                        <button class='btn btn-danger' style='margin-left: 5px; float: right' onclick="deletePostBtnClicked('${encodeURIComponent(
+                          JSON.stringify(post)
+                        )}')">delete</button>
+
+                        <button class='btn btn-secondary' style='float: right' onclick="editPostBtnClicked('${encodeURIComponent(
+                          JSON.stringify(post)
+                        )}')">edit</button>
+                    `;
+        }
+
         let content = `<div class="card shadow ">
                         <div class="card-header">
+                        
+                            <span onclick="userClicked(${
+                              post.author.id
+                            })" style="cursor: pointer;"> 
                             <img class="rounded-circle border border-2" src="${
                               post.author.profile_image
                             }" alt=""
                                 style="width: 40px; height: 40px;">
                             <b>${post.author.name}</b>
+                            </span>
+                                                                            ${editBtnContent}
+
+
                         </div>
-                        <div class="card-body">
+                        <div class="card-body" onclick ="postClick(${
+                          post.id
+                        })" style="cursor: pointer">
                             <img class="w-100" src="${post.image}" alt="">
                             <h6 class="mt-2" style="color: rgb(163, 159, 159);">${
                               post.created_at
@@ -86,31 +115,162 @@ function getPosts() {
     });
 }
 
+//toggleLoader
+function toggleLoader(show = true) {
+  if (show) {
+    document.getElementById("loader").style.visibility = "visible";
+  } else {
+    document.getElementById("loader").style.visibility = "hidden";
+  }
+}
+
+// Function to toggle dark mode and store preference in localStorage
+function toggleDarkMode() {
+  var body = document.body;
+  var icon = document.getElementById("toggleIcon");
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    icon.src = "image/dark.png";
+    icon.alt = "Dark Mode";
+    localStorage.setItem("mode", "dark");
+  } else {
+    icon.src = "image/light.png";
+    icon.alt = "Light Mode";
+    localStorage.setItem("mode", "light");
+  }
+}
+
+// Function to load the stored mode from localStorage
+function loadStoredMode() {
+  var storedMode = localStorage.getItem("mode");
+  var body = document.body;
+  var icon = document.getElementById("toggleIcon");
+
+  if (storedMode === "dark") {
+    body.classList.add("dark-mode");
+    icon.src = "image/dark.png";
+    icon.alt = "Dark Mode";
+  } else {
+    icon.src = "image/light.png";
+    icon.alt = "Light Mode";
+  }
+}
+
+// Load the stored mode when the page loads
+window.onload = loadStoredMode;
+
 // create new post
 function createNewPostClicked() {
+  let postId = document.getElementById("post-id-input").value;
+  let isCreate = postId == null || postId == "";
+
   const title = document.getElementById("post-title-input").value;
   const body = document.getElementById("post-body-input").value;
-  const image = document.getElementById("post-image-input").files[0]; //get first image using file(s)
+  const image = document.getElementById("post-image-input").files[0];
   const token = localStorage.getItem("token");
-  //to send form data not json
+
   let formData = new FormData();
   formData.append("body", body);
   formData.append("title", title);
   formData.append("image", image);
 
+  let url = ``;
   const headers = {
-    "Content-Type": "multipart/from-data",
+    "Content-Type": "multipart/form-data",
     authorization: `Bearer ${token}`,
   };
+
+  if (isCreate) {
+    url = `${baseUrl}/posts`;
+  } else {
+    formData.append("_method", "put");
+    url = `${baseUrl}/posts/${postId}`;
+  }
+
+  toggleLoader(true);
   axios
-    .post(`https://tarmeezacademy.com/api/v1/posts`, formData, {
+    .post(url, formData, {
       headers: headers,
     })
     .then((response) => {
       const modal = document.getElementById("create-post-modal");
-      const modalinstance = bootstrap.Modal.getInstance(modal);
-      modalinstance.hide();
+      const modalInstance = bootstrap.Modal.getInstance(modal);
+      modalInstance.hide();
       showAlert("New Post Has Been Created", "success");
+      getPosts();
+    })
+    .catch((error) => {
+      const message = error.response.data.message;
+      showAlert(message, "danger");
+    })
+    .finally(() => {
+      toggleLoader(false);
+    });
+}
+
+function editPostBtnClicked(postObject) {
+  let postModal = new bootstrap.Modal(
+    document.getElementById("create-post-modal"),
+    {}
+  );
+  postModal.toggle();
+  let post = JSON.parse(decodeURIComponent(postObject));
+  console.log(post);
+  document.getElementById("post-id-input").value = post.id;
+
+  document.getElementById("post-modal-title").innerHTML = "Edit Post";
+  document.getElementById("post-title-input").value = post.title;
+  document.getElementById("post-body-input").value = post.body;
+  document.getElementById("post-modal-submit-btn").innerHTML = "Update";
+}
+
+function addBtnClicked() {
+  document.getElementById("post-modal-submit-btn").innerHTML = "Create";
+  document.getElementById("post-id-input").value = "";
+  document.getElementById("post-modal-title").innerHTML = "Create A New Post";
+  document.getElementById("post-title-input").value = "";
+  document.getElementById("post-body-input").value = "";
+  let postModal = new bootstrap.Modal(
+    document.getElementById("create-post-modal"),
+    {}
+  );
+  postModal.toggle();
+}
+
+// delete
+
+function deletePostBtnClicked(postObject) {
+  let post = JSON.parse(decodeURIComponent(postObject));
+  console.log(post);
+
+  document.getElementById("delete-post-id-input").value = post.id;
+  let postModal = new bootstrap.Modal(
+    document.getElementById("delete-post-modal"),
+    {}
+  );
+  postModal.toggle();
+}
+
+// confirm delete
+function confirmPostDelete() {
+  const token = localStorage.getItem("token");
+  const postId = document.getElementById("delete-post-id-input").value;
+  const url = `${baseUrl}/posts/${postId}`;
+  const headers = {
+    "Content-Type": "multipart/form-data",
+    authorization: `Bearer ${token}`,
+  };
+
+  axios
+    .delete(url, {
+      headers: headers,
+    })
+    .then((response) => {
+      const modal = document.getElementById("delete-post-modal");
+      const modalInstance = bootstrap.Modal.getInstance(modal);
+      modalInstance.hide();
+      showAlert("The Post Has Been Deleted Successfully", "success");
       getPosts();
     })
     .catch((error) => {
@@ -118,48 +278,3 @@ function createNewPostClicked() {
       showAlert(message, "danger");
     });
 }
-
-//toggleLoader 
-function toggleLoader(show = true) {
-  if (show) {
-    document.getElementById("loader").style.visibility = "visible";
-  }else{
-    document.getElementById("loader").style.visibility = "hidden";
-}
-}
-
-// Function to toggle dark mode and store preference in localStorage
-function toggleDarkMode() {
-    var body = document.body;
-    var icon = document.getElementById("toggleIcon");
-    body.classList.toggle("dark-mode");
-
-    if (body.classList.contains("dark-mode")) {
-        icon.src = "image/dark.png"; 
-        icon.alt = "Dark Mode";
-        localStorage.setItem('mode', 'dark');
-    } else {
-        icon.src = "image/light.png";
-        icon.alt = "Light Mode";
-        localStorage.setItem('mode', 'light');
-    }
-}
-
-// Function to load the stored mode from localStorage
-function loadStoredMode() {
-    var storedMode = localStorage.getItem('mode');
-    var body = document.body;
-    var icon = document.getElementById("toggleIcon");
-
-    if (storedMode === 'dark') {
-        body.classList.add("dark-mode");
-        icon.src = "image/dark.png"; 
-        icon.alt = "Dark Mode";
-    } else {
-        icon.src = "image/light.png";  
-        icon.alt = "Light Mode";
-    }
-}
-
-// Load the stored mode when the page loads
-window.onload = loadStoredMode;
